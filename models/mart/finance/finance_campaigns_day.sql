6b72688ffbf321431393fef7db4ca9c0a82a5328
Emmanouil Siniorakis <manostimy@gmail.com>
2024-01-10 14:12:00 +0000

    joined campaigns and finance in one table

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,4 @@
+SELECT *
+    ,operational_margin-ads_cost as ads_margin
+FROM {{ref("int_campaigns_day")}}
+JOIN {{ref("finance_days")}} USING (date_date)
--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -3,7 +3,7 @@ SELECT date_date
     ,ROUND(SUM(revenue),2) AS total_revenue
     ,ROUND(SUM(revenue)/COUNT(orders_id),2) AS average_basket
     ,ROUND(SUM(margin),2) AS margin
-    ,ROUND(SUM(operational_margin),2) AS operationa_margin
+    ,ROUND(SUM(operational_margin),2) AS operational_margin
     ,SUM(quantity) AS quantity
 FROM {{ref('int_orders_operational')}}
 GROUP BY date_date
