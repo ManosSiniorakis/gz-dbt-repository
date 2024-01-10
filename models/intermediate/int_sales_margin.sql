9c4b348ef21fb8cb81c7e74253c0649247896179
Emmanouil Siniorakis <manostimy@gmail.com>
2024-01-10 12:57:06 +0000

    created staged raw data for bing, facebook, criteo and adwords

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,5 @@
 SELECT *
     ,CAST(revenue AS INT64) - CAST(purchase_price AS float64) AS margin
     ,CAST(quantity AS INT64) * CAST(purchase_price AS float64) AS purchase_cost
-FROM  {{ ref('sales')}}
-JOIN {{ ref('product')}} USING (products_id)
+FROM  {{ ref('stg_raw__sales')}}
+JOIN {{ ref('stg_raw__product')}} USING (products_id)
