17aa0d92f43651807f58e19371f6869dd34740eb
Emmanouil Siniorakis <manostimy@gmail.com>
2024-01-11 08:13:06 +0000

    implemented the function {{create_margin_percent}} in my int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,6 @@
 SELECT *
-    ,CAST(revenue AS INT64) - CAST(purchase_price AS float64) AS margin
-    ,CAST(quantity AS INT64) * CAST(purchase_price AS float64) AS purchase_cost
-    ,{{create_margin_percent('revenue', 'purchase_price')}} as margin_percent
-FROM  {{ ref('stg_raw__sales')}} 
-JOIN {{ ref('stg_raw__product')}} USING (products_id)
+    ,CAST(s.revenue AS INT64) - CAST(p.purchase_price AS float64) AS margin
+    ,CAST(s.quantity AS INT64) * CAST(p.purchase_price AS float64) AS purchase_cost
+    ,{{create_margin_percent('revenue','s.quantity*CAST(p.purchase_price AS FLOAT64)')}} as margin_percent
+FROM  {{ ref('stg_raw__sales')}} as s
+JOIN {{ ref('stg_raw__product')}} as p USING (products_id)
