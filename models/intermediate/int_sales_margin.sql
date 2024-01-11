aa9e2854313bf7d59734a43b7fe5f8a49dd4f5f3
Emmanouil Siniorakis <manostimy@gmail.com>
2024-01-11 08:01:26 +0000

    updated the macro function for create_margin_percent

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,6 @@
 SELECT *
     ,CAST(revenue AS INT64) - CAST(purchase_price AS float64) AS margin
     ,CAST(quantity AS INT64) * CAST(purchase_price AS float64) AS purchase_cost
-FROM  {{ ref('stg_raw__sales')}}
+    ,{{create_margin_percent('revenue', 'purchase_price')}} as margin_percent
+FROM  {{ ref('stg_raw__sales')}} 
 JOIN {{ ref('stg_raw__product')}} USING (products_id)
